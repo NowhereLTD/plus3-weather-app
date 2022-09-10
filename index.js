c163c3a93d6564fe7637fa8bf587241382652b0d
Mary Zzz <110771412+consueloenlatristeza@users.noreply.github.com>
2022-09-10 19:59:43 +0200

    add js and css

---
+++ index.js
@@ -0,0 +1,5 @@
+let hometown = prompt("What is your hometown?");
+alert(hometown);
+let firstName = prompt("What is your  first name?");
+let lastName = prompt("What is your  last name?");
+alert(firstName + lastName);
